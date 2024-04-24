163583d8351e6644337845355e8f1bc9f86203e9
Nilaksha Rathnayake <nilaksha2005@gmail.com>
2024-04-25 01:16:56 +0530

    fix build time errors

--- app/hac-portal/page.tsx
+++ app/hac-portal/page.tsx
@@ -84,15 +84,18 @@ const HACTable = (data: Incident[]) => {
 };
 
 export default async function HACPortal() {
-  // const data = await getData();
+  const data = envConfig.API_URL ? await getData() : {};
 
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <Sidebar brandLabel="Rethink Wildlife" />
       <div className="flex flex-col">
         <Header brandLabel="Rethink Wildlife" />
-        {/* <Main title="HAC Portal" tiles={HACTable(data.rows)} /> */}
-        <Main title="HAC Portal" tiles={<></>} />
+        {envConfig.API_URL ? (
+          <Main title="HAC Portal" tiles={HACTable(data.rows)} />
+        ) : (
+          <Main title="HAC Portal" tiles={<></>} />
+        )}
       </div>
     </div>
   );
